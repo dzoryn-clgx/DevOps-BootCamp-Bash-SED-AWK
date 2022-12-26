f284924857e23365c82c9f0b6b18abc8e107294e
Dzmitry Zoryn <105269360+dzoryn-clgx@users.noreply.github.com>
2022-12-26 10:58:34 +0400

    Update sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -2,8 +2,9 @@
 
 
 cp ./passwd ./passwd_new
-awk 'BEGIN{FS=":";OFS=":"} $1=="saned" {$NF="/bin/bash"}1' passwd_new > tmp && mv tmp passwd_new
-sed -i '/avahi:/s@/usr/sbin/nologin@/bin/bash@' passwd_new
-awk 'BEGIN{FS=":";OFS=":"} {print $1,$3,$5,$7 > "passwd_new"}' passwd_new
-sed -i '/daemon/d' passwd_new
-awk 'BEGIN{FS=":";OFS=":"} $2%2==0 {$NF="/bin/zsh"}1' passwd_new > tmp && mv tmp passwd_new
+file="./passwd_new"
+awk 'BEGIN{FS=":";OFS=":"} $1=="saned" {$NF="/bin/bash"}1' "${file}" > tmp && mv tmp "${file}"
+sed -i '/avahi:/s@/usr/sbin/nologin@/bin/bash@' "${file}"
+awk 'BEGIN{FS=":";OFS=":"} {print $1,$3,$5,$7 > ""${file}""}' "${file}"
+sed -i '/daemon/d' "${file}"
+awk 'BEGIN{FS=":";OFS=":"} $2%2==0 {$NF="/bin/zsh"}1' "${file}" > tmp && mv tmp "${file}"
