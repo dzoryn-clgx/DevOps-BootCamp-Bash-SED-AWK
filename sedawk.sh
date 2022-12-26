2b6017dacee77a28ac118571c77af075336927b9
Dzmitry Zoryn <105269360+dzoryn-clgx@users.noreply.github.com>
2022-12-26 12:40:31 +0400

    Update sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -5,7 +5,7 @@ cp ./passwd ./passwd_new
 
 
 # b. Change shell for user saned from /usr/sbin/nologin to /bin/bash using AWK
-awk 'BEGIN{FS=":";OFS=":"} $1=="saned" {$NF="/bin/bash"}1' passwd_new > tmp && mv tmp passwd_new
+awk  -i inplace 'BEGIN{FS=":";OFS=":"} $1=="saned" {$NF="/bin/bash"}1' passwd_new
 
 # c. Change shell for user avahi from /usr/sbin/nologin to /bin/bash using SED
 sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' ./passwd_new
@@ -17,4 +17,4 @@ awk 'BEGIN{FS=":";OFS=":"} {print $1,$3,$5,$7 > "passwd_new"}' passwd_new
 sed -i '/daemon/d' ./passwd_new
 
 # f. Change shell for all users with even UID to /bin/zsh
-awk 'BEGIN{FS=":";OFS=":"} $2%2==0 {$NF="/bin/zsh"}1' passwd_new > tmp && mv tmp passwd_new
+awk  -i inplace 'BEGIN{FS=":";OFS=":"} $2%2==0 {$NF="/bin/zsh"}1' passwd_new 
