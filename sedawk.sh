e26afd6255fa5db687076f55b0cdbbc0c4cfe188
Dzmitry Zoryn <105269360+dzoryn-clgx@users.noreply.github.com>
2022-12-26 11:58:30 +0400

    Update sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -5,7 +5,7 @@ cp ./passwd ./passwd_new
 
 
 # b. Change shell for user saned from /usr/sbin/nologin to /bin/bash using AWK
-awk -i inplace -F':' -OFS':' '/saned/{gsub("/usr/sbin/nologin","/bin/bash") }; {print}' ./passwd_new
+awk 'BEGIN{FS=":";OFS=":"} $1=="saned" {$NF="/bin/bash"}1' passwd_new > tmp && mv tmp passwd_new
 
 # c. Change shell for user avahi from /usr/sbin/nologin to /bin/bash using SED
 sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' ./passwd_new
